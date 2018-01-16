388993e316bc739f085b6327228639553ba2033d
Eric Scuccimarra <skooch@gmail.com>
2018-01-16 18:34:41 +0100

    Shiny App

--- CourseProject/server.R
+++ CourseProject/server.R
@@ -1,26 +1,44 @@
 #
-# This is the server logic of a Shiny web application. You can run the 
+# This is the server logic of a Shiny web application. You can run the
 # application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
 library(shiny)
+# Load the data
+data(esoph)
+# Add the rate for the group
+esoph$rate <- esoph$ncases / (esoph$ncases + esoph$ncontrols)
+# Fit a model
+fit <- lm(rate ~ agegp + alcgp + tobgp, data= esoph)
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
-  
+
+    predictRate <- reactive({
+        prediction <- predict(fit, newdata = data.frame(agegp= input$agegp, alcgp=input$alcgp, tobgp=input$tobgp), interval="prediction")
+        # Update the results to eliminate negatives
+        if(prediction[1] < 0 ){
+            prediction[1] <- 0
+        }
+        if(prediction[2] < 0){
+            prediction[2] <- 0
+        }
+        if(prediction[3] < 0){
+            prediction[3] <- 0
+        }
+        prediction <- round(prediction * 100, 2)
+    })
+
+    output$prediction <- renderText({
+        paste(round(predictRate()[1], 2), "%")
+    })
+    output$confInt <- renderText({
+        paste(predictRate()[2], '% to ', predictRate()[3], ' %')
+    })
+
 })
--- CourseProject/ui.R
+++ CourseProject/ui.R
@@ -3,31 +3,42 @@
 # run the application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
 library(shiny)
+data(esoph)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
+
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
+  titlePanel("Predictions of Esophogeal Cancer From Age, Alcohol and Nicotine Consumption"),
+
+  # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+       selectInput("agegp",
+                   "Please select your age group:",
+                   list('25-34','35-44','45-54','55-64','65-74','75+')),
+       selectInput("alcgp",
+                   "Please select your alcohol consumption:",
+                   list('0-39g/day','40-79','80-119','120+')),
+       selectInput("tobgp",
+                   "Please select your tobacco consumption:",
+                   list('0-9g/day','10-19','20-29','30+'))
     ),
-    
+
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+        h3("Your chances of having esophogeal cancer:"),
+        textOutput("prediction"),
+        h5("With a confidence interval of:"),
+        textOutput("confInt"),
+        br(),
+        p("This estimate is based on a case-control study of esophageal cancer in Ille-et-Vilaine, France in 1980."),
+        p("This data is merely a prediction based on the results of the study, it should not be used for any medical purposes. If you are worried about esophogeal cancer you should see a doctor.")
     )
   )
 ))
